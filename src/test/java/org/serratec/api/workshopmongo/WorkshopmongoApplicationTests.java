076f5a4d785b1477592ec5f37aff83d4eb2d2bf0
Felipe Almeida <falmeida.1305@gmail.com>
2021-07-05 19:53:46 -0300

    Projeto criado

---
+++ src/test/java/org/serratec/api/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.api.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
