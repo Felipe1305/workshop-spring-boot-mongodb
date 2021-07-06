de79f60cdd7984e48e0289bd748dc100fb8f00af
Felipe Almeida <falmeida.1305@gmail.com>
2021-07-06 14:46:03 -0300

    Referenciando os posts do usuário

--- src/main/java/org/serratec/api/workshopmongo/config/Instantiation.java
+++ src/main/java/org/serratec/api/workshopmongo/config/Instantiation.java
@@ -40,6 +40,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
 
 		postRepo.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepo.save(maria);
 	}
 
 }
