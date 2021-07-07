4db50cee011e9796a26eaf8c8fafffd84d97cf02
Felipe Almeida <falmeida.1305@gmail.com>
2021-07-06 23:52:57 -0300

    Consulta simples com @Query

--- src/main/java/org/serratec/api/workshopmongo/repositories/PostRepository.java
+++ src/main/java/org/serratec/api/workshopmongo/repositories/PostRepository.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.serratec.api.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String title);
+	
+	@Query("{'title': {$regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String title);
 
 }
